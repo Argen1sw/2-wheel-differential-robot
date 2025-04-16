06e33d7813cb0b3267521def55e3ecc976184602
Argenis Wong <argen1swong@gmail.com>
2025-04-16 02:33:41 +0000

    removed install, logs and build directories added by mistake

--- build/ld08_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/ros2-slam-basic/src/ld08_driver
-# Build directory: /home/ubuntu/ros2-slam-basic/build/ld08_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/slam_toolbox/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/ros2-slam-basic/src/slam_toolbox
-# Build directory: /home/ubuntu/ros2-slam-basic/build/slam_toolbox
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/karto_sdk")
-subdirs("slam_toolbox__py")
